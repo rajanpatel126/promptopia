f2c935918cd3ae08a19138fd87eea006e32a37c6
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2023-06-04 22:23:10 +0530

    Google based authentication done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -16,35 +16,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
+    async signIn({ profile }) {
+      try {
+        //server less route -> Lambda Function -> opens up only when it gets called
+        await connectToDb();
+        //check if user already exits
+        const userExist = await User.findOne({ email: profile.email });
 
-  async signIn({ profile }) {
-    try {
-      //server less route -> Lambda Function -> opens up only when it gets called
-      await connectToDb();
-      //check if user already exits
-      const userExist = await User.findOne({ email: profile.email });
-
-      //if not, create a user
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          userName: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        //if not, create a user
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            userName: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,51 +1,55 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { signOut, getProviders, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
-  const [toggleDropDown, setToggleDropDown] = useState(false);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    };
-    setProviders();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
 
   return (
-    <nav className="flex-between w-full mb-18 pt-3">
-      <Link href={"/"} className="flex gap-2 flex-center">
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
+          alt="logo"
           width={30}
           height={30}
-          alt="Proptopia Logo"
           className="object-contain"
         />
         <p className="logo_text">Promptopia</p>
       </Link>
-      {/* mobile Navigation */}
+
+      {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
+
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
-            <Link href={"/profile"}>
+
+            <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
-                alt="profile"
                 className="rounded-full"
+                alt="profile"
               />
             </Link>
           </div>
@@ -56,50 +60,54 @@ const Nav = () => {
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
         )}
       </div>
-      {/* mobile navigation */}
+
+      {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
+              className="rounded-full"
               alt="profile"
-              className="rounded-full cursor-pointer"
-              onClick={() => setToggleDropDown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
-            {toggleDropDown && (
+
+            {toggleDropdown && (
               <div className="dropdown">
                 <Link
                   href="/profile"
                   className="dropdown_link"
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
                   href="/create-prompt"
                   className="dropdown_link"
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
                 </Link>
                 <button
                   type="button"
                   onClick={() => {
-                    setToggleDropDown(false);
+                    setToggleDropdown(false);
                     signOut();
                   }}
-                  className="mt-5 black_btn w-full cursor-pointer"
+                  className="mt-5 w-full black_btn"
                 >
                   Sign Out
                 </button>
@@ -113,10 +121,12 @@ const Nav = () => {
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
